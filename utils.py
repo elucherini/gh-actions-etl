b432ac880e3a56145a0da35945f6d8812cb7b2bf
Eli Lucherini <eleluche@gmail.com>
2024-12-21 10:19:00 -0800

    Fix

--- utils.py
+++ utils.py
@@ -62,24 +62,3 @@ def lightly_parse_data(data: Dict[str, Any]) -> List[SFAddress]:
         addresses.append(address)
 
     return addresses
-
-
-def save_to_csv(data: List[SFAddress], filename: str):
-    """
-    Save the data to a CSV file.
-
-    Args:
-        data (list): The data to save.
-        filename (str): The name of the CSV file.
-    """
-    with open(filename, mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(SFAddress.__annotations__.keys())
-        for address in data:
-            writer.writerow(address.__dict__.values())
-
-
-if __name__ == "__main__":
-    data = fetch_data(URL)
-    address_data = lightly_parse_data(data)
-    save_to_csv(address_data, "output.csv")
